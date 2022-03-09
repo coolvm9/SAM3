f1b1f28b4c079632d19d1f76e48bd9e2233c0d4b
koo vemu <coolvm9@gmail.com>
2022-03-08 17:42:27 -0800

    Initial commit

---
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -0,0 +1,25 @@
+package helloworld;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.example.Book;
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void successfulResponse() {
+    App app = new App();
+    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    Book book = new Book();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
---
+++ Mod2/src/main/java/com/example/Book.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Book {
+
+    public String getTest() {
+        return "Hellow Nee Parri";
+    }
+
+
+
+    private String test;
+
+
+
+}
